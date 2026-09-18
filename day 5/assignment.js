4134a945001feef1945870221f081c5a96ca6d76
hala-abuelsoud <hala.elsoud@xpin.ae>
2026-09-18 17:20:33 +0400

    Add JavaScript file for Day 5 assignment: implement student marks calculation and grading functions

---
+++ day 5/assignment.js
@@ -0,0 +1,91 @@
+// // // Create variables for a student's name and marks in 3 subjects.
+
+// // let name= "hala";
+
+// // let english= 80;
+// // let arabic= 60;
+// // let math= 88;
+
+// // //  Calculate the student's total marks.
+
+// // let sum= english+ arabic + math;
+// // console.log(sum);
+
+// // //  Calculate the student's average marks.
+
+// // let avg= sum/3; 
+// // console.log(avg);
+
+// // //  Check if the student passed or failed using if/else.
+// // let result;
+
+// // if (avg <50){
+
+// //     result = ("fail")
+// // } else{
+// //     result= ("pass")
+// // }
+
+// // console.log(result);
+
+// // //  If the average is 50 or above, print "Passed", otherwise print "Failed".
+
+// // //  Print the student's name, total, average, and result.
+
+// // console.log(name);
+// // console.log(sum);
+// // console.log(avg);
+// // console.log(result);
+// // //  Create a function to calculate the total marks.
+
+// // function totalMarks( english, arabic, math){
+
+// //     let total;
+// //     total = english + arabic + math;
+// //     console.log(total)
+
+// // }
+// // console.log(totalMarks(english, arabic, math));
+
+
+// // //  Create a function that accepts two numbers and returns their sum.
+
+// var num1= 5;
+// var num2= 9;
+
+//  function totalSum(num1 , num2){
+   
+//     console.log(num1 + num2);
+// }
+
+// console.log(totalSum(num1,num2));
+// // //  What will be the output of your program?
+
+
+
+// //  How would you modify the program to calculate the grade (A, B, C, D, F)?
+marks();
+// var marks => function (subject){
+
+//var getname=function();
+//     if (subject >= 90){
+//          return "A";
+//     }
+//     else if(subject >= 80){
+//         return "B";
+//     }
+//     else if(subject >= 70){
+//         return "C";
+//     }
+//     else if(subject >= 60){
+//         return "D";
+//     }
+//     else {
+//         return "F";
+//     }
+
+// }
+
+
+// console.log(marks(80));
+
