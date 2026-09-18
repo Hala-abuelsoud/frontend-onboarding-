35db1d0fc15705dfd7b7c941d9386a7ebef79d0f
hala-abuelsoud <hala.elsoud@xpin.ae>
2026-09-18 10:24:58 +0400

    added day 4 task

---
+++ day 4/task.js
@@ -0,0 +1,17 @@
+
+// What is Node.js?
+// Is Node.js a programming language, library, framework, or runtime?
+// How is Node.js different from JavaScript?
+// How does Node.js run JavaScript outside the browser?
+// What is the V8 engine?
+// Why can Node.js access files and the operating system?
+// Why doesn't Node.js have window or document like Chrome?
+// How do you run a JavaScript file using Node.js in VS Code?
+// How do you check if Node.js is installed on your system?
+// What command is used to run a JavaScript file with Node.js?
+
+const x=3;
+const y=4;
+let z= y + x;
+
+console.log(z+ "total of z is");
